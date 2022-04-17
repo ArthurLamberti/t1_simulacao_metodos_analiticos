ae48980ae9971735a9be31134241df410a437b74
arthurlamberti <arthurlamberti001@gmail.com>
2022-04-17 17:03:48 -0300

    gambi monstruosa

--- src/YmlReader/YmlReader.java
+++ src/YmlReader/YmlReader.java
@@ -1,15 +1,22 @@
 package YmlReader;
 
+import com.sun.tools.javac.Main;
+
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class YmlReader {
     public YmlReader(){}
 
     public Map<String, SimulationConfig> readFile(){
-        File file = new File("src/config.txt");
+        File file = getFileByGambi();
+
         DecodeFile decodeFile = new DecodeFile();
         Map<String, SimulationConfig> simulationMap = null;
         try {
@@ -20,6 +27,21 @@ public class YmlReader {
             e.printStackTrace();
         }
         return simulationMap;
+    }
+
+    private File getFileByGambi(){
+        URL url = this.getClass().getResource("YmlReader.class");
+        String[] paths = url.getPath().split("\\/");
+        List<String> configPath = new ArrayList<>();
+        int count = 0;
+        for (int i = 0; !paths[i].equals("out"); i++) {
+            configPath.add(paths[i]);
+        }
+        configPath.add("src");
+        configPath.add("config.txt");
+        System.out.println(url.getPath());
+
+        return new File(String.join("/", configPath));
     }
 
 }
